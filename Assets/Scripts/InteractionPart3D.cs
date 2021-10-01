02d72902838a4dbbaccb7c55da4bc211d1bf4086
BigSpaceships <bigspaceships56@gmail.com>
2021-10-01 19:02:51 -0400

    Created the project

---
+++ Assets/Scripts/InteractionPart3D.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionPart3D : MonoBehaviour {
+    public Collider collider;
+    void Start() {
+        if (collider == null) {
+            if (TryGetComponent(typeof (Collider), out Component collider)) {
+                this.collider = (Collider) collider;
+            } else {
+                Debug.LogError("InteractionPart3D needs a collider attached to the object");
+            }
+        }
+
+        gameObject.layer = 9;
+    }
+
+    public void Interact() {
+        Debug.Log("Touch");
+    }
+}
